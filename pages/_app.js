39b2c45e65478cc617ec9740175a850c90bd10f8
Julius von Bonin <167006924+Tolpius@users.noreply.github.com>
2025-08-21 14:24:20 +0200

    swrconfig

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import GlobalStyle from "../styles";
-
+import { SWRConfig } from "swr";
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (resource, init) =>
+          fetch(resource, init).then((res) => res.json()),
+      }}
+    >
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </SWRConfig>
   );
 }
