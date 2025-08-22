21fa19a153d653641e60f7deeb8c1ef2e2de092b
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-08-22 12:28:05 +0200

    implement backbutton

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,10 +1,15 @@
 import Link from "next/link";
-
-
+import styled from "styled-components";
+import { ArrowCircleLeftIcon } from "@phosphor-icons/react";
 
 export default function BackButton() {
-  return <Link href={"/"}>Back</Link>;
+  return (
+    <StyledBackButton href={"/"}>
+      <ArrowCircleLeftIcon size={32} />
+    </StyledBackButton>
+  );
 }
 
-
-
+const StyledBackButton = styled(Link)`
+  text-decoration: none;
+`;
