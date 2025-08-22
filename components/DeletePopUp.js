d1dea5c88715aba27fbd4575fd00cf5df22b212e
Milia Laufenberg <coding.l@ufenberg.net>
2025-08-22 16:51:49 +0200

    delete is working

---
+++ components/DeletePopUp.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export default function DeletePopUp({ deletePlant, setShowPopUp }) {
+  return (
+    <StyledPopUpWarning>
+      <StyledWarningMessage>
+        Are you sure you want to delete this plant? This decision is not
+        reversible!
+      </StyledWarningMessage>
+      <ButtonWrapper>
+        <StyledCancelButton
+          onClick={() => {
+            setShowPopUp(false);
+          }}
+        >
+          cancel
+        </StyledCancelButton>
+        <StyledDeleteButton onClick={deletePlant}>delete</StyledDeleteButton>
+      </ButtonWrapper>
+    </StyledPopUpWarning>
+  );
+}
+
+const StyledPopUpWarning = styled.article`
+  display: flex;
+  flex-direction: column;
+  z-index: 2;
+  position: fixed;
+  width: 80%;
+  height: 40%;
+  padding: 20px;
+  background-color: lightgray;
+  border-radius: 25px;
+  box-shadow: 5px 5px 15px;
+`;
+
+const StyledWarningMessage = styled.p`
+  padding: 20px;
+  padding-top: 40px;
+  font-size: large;
+  text-align: center;
+  height: 60%;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  height: 30px;
+`;
+
+const StyledDeleteButton = styled.button`
+  font-size: medium;
+  background-color: lightgray;
+  width: 100px;
+  border-radius: 5px;
+`;
+
+const StyledCancelButton = styled.button`
+  width: 100px;
+  border-radius: 5px;
+  background-color: darkslategray;
+  color: white;
+  font-size: medium;
+`;
--- pages/plants/[id]/index.js
+++ pages/plants/[id]/index.js
@@ -1,9 +1,11 @@
 import BackButton from "@/components/BackButton";
+import DeletePopUp from "@/components/DeletePopUp";
 
 import Image from "next/image";
 import { useRouter } from "next/router";
 import styled from "styled-components";
 import useSWR from "swr";
+import { useState } from "react";
 
 export default function DetailsPage() {
   const router = useRouter();
@@ -11,76 +13,120 @@ export default function DetailsPage() {
   const { id } = router.query;
 
   const { data: plant, isLoading, error } = useSWR(`/api/plants/${id}`);
+  const [showPopUp, setShowPopUp] = useState(false);
 
   if (!isReady || isLoading || error || !plant) return <h2>Loading...</h2>;
 
   const seasons = plant.fertiliserSeason;
 
+  async function deletePlant() {
+    const response = await fetch(`/api/plants/${id}`, { method: "DELETE" });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
+
   return (
     <>
-      <BackButton />
+      
+        <BackButton />
+        <StyledImage
+          src={plant.imageUrl}
+          alt={plant.name}
+          width={300}
+          height={0}
+        />
+        <NameWrapper>
+          <StyledPlantName>{plant.name}</StyledPlantName>
+          <StyledBotanicalName>{plant.botanicalName}</StyledBotanicalName>
+        </NameWrapper>
+        <p>{plant.description}</p>
+        <h4>Care</h4>
+        <StyledInfoRow>
+          <StyledCareInfo>Plant likes:</StyledCareInfo>
+          <StyledCareInfo>
+            {plant.lightNeed === "1"
+              ? "⛅ "
+              : plant.lightNeed === "2"
+              ? "🌤️  "
+              : plant.lightNeed === "3"
+              ? "☀️ "
+              : plant.lightNeed}
+          </StyledCareInfo>
+        </StyledInfoRow>
+        <StyledInfoRow>
+          <StyledCareInfo>Water need:</StyledCareInfo>
+          <StyledCareInfo>
+            {plant.waterNeed === "1"
+              ? "💧 "
+              : plant.waterNeed === "2"
+              ? "💧💧 "
+              : plant.waterNeed === "3"
+              ? "💧💧💧 "
+              : plant.waterNeed}
+          </StyledCareInfo>
+        </StyledInfoRow>
+        <StyledInfoRow>
+          <StyledCareInfo>Fertilise in:</StyledCareInfo>
 
-      <StyledImage
-        src={plant.imageUrl}
-        alt={plant.name}
-        width={300}
-        height={0}
-      />
-      <NameWrapper>
-        <StyledPlantName>{plant.name}</StyledPlantName>
-        <StyledBotanicalName>{plant.botanicalName}</StyledBotanicalName>
-      </NameWrapper>
-      <p>{plant.description}</p>
-      <h4>Care</h4>
-      <StyledInfoRow>
-        <StyledCareInfo>Plant likes:</StyledCareInfo>
-        <StyledCareInfo>
-          {plant.lightNeed === "1"
-            ? "⛅ "
-            : plant.lightNeed === "2"
-            ? "🌤️  "
-            : plant.lightNeed === "3"
-            ? "☀️ "
-            : plant.lightNeed}
-        </StyledCareInfo>
-      </StyledInfoRow>
-      <StyledInfoRow>
-        <StyledCareInfo>Water need:</StyledCareInfo>
-        <StyledCareInfo>
-          {plant.waterNeed === "1"
-            ? "💧 "
-            : plant.waterNeed === "2"
-            ? "💧💧 "
-            : plant.waterNeed === "3"
-            ? "💧💧💧 "
-            : plant.waterNeed}
-        </StyledCareInfo>
-      </StyledInfoRow>
-      <StyledInfoRow>
-        <StyledCareInfo>Fertilise in:</StyledCareInfo>
+          {seasons.map((season) => {
+            return (
+              <li key={season}>
+                <StyledCareInfo>
+                  {season === "spring"
+                    ? "🌸 Spring"
+                    : season === "summer"
+                    ? "☀️ Summer"
+                    : season === "autumn"
+                    ? "🍂 Autumn"
+                    : season === "winter"
+                    ? "❄️ Winter"
+                    : season}
+                </StyledCareInfo>
+              </li>
+            );
+          })}
+        </StyledInfoRow>
+        <StyledDeleteButton
+          onClick={() => {
+            setShowPopUp(true);
+          }}
+        >
+          Delete
+        </StyledDeleteButton>{" "}
+      
 
-        {seasons.map((season) => {
-          return (
-            <li key={season}>
-              <StyledCareInfo>
-                {season === "spring"
-                  ? "🌸 Spring"
-                  : season === "summer"
-                  ? "☀️ Summer"
-                  : season === "autumn"
-                  ? "🍂 Autumn"
-                  : season === "winter"
-                  ? "❄️ Winter"
-                  : season}
-              </StyledCareInfo>
-            </li>
-          );
-        })}
-      </StyledInfoRow>
+      {showPopUp && (
+        <TestOverlay>
+          <DeletePopUp deletePlant={deletePlant} setShowPopUp={setShowPopUp} />
+        </TestOverlay>
+      )}
     </>
   );
 }
 
+const TestOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent background */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 1;
+`;
+
+
+const StyledDeleteButton = styled.button`
+background-color: red;
+color: white;
+border-radius: 5px;
+height: 30px;
+margin-top: 30px;
+
+`
 const StyledImage = styled(Image)`
   width: 100%;
   height: auto;
