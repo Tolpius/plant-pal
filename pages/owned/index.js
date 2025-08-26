791ba8df3ffd49563a7992145016478f40f2b350
dominikmusewitsch <dmusewitsch@gmail.com>
2025-08-26 10:03:02 +0200

    add filter component to owned page

--- pages/owned/index.js
+++ pages/owned/index.js
@@ -1,10 +1,14 @@
 import useSWR from "swr";
 import useLocalStorage from "use-local-storage";
+import { useState } from "react";
 import PlantList from "@/components/PlantList";
+import PlantFilter from "@/components/filter/PlantFilter";
+import PlantCounter from "@/components/PlantCounter";
 
 export default function Owned() {
   const { data, isLoading } = useSWR("/api/plants");
   const [ownedPlantIds] = useLocalStorage("ownedPlantIds", []);
+  const [filteredPlants, setFilteredPlants] = useState();
 
   if (isLoading) {
     return <p>Loading...</p>;
@@ -14,6 +18,7 @@ export default function Owned() {
     return <p>Failed to load plants!</p>;
   }
   const ownedData = data.filter((plant) => ownedPlantIds.includes(plant._id));
+
   if (ownedData.length == 0) {
     return (
       <p>
@@ -26,5 +31,14 @@ export default function Owned() {
       </p>
     );
   }
-  return <PlantList plants={ownedData} />;
+
+  const filteredPlantList = filteredPlants ?? ownedData;
+
+  return (
+    <>
+      <PlantFilter data={ownedData} setFilteredPlants={setFilteredPlants} />
+      <PlantCounter length={filteredPlantList.length} />
+      <PlantList plants={filteredPlantList} />
+    </>
+  );
 }
