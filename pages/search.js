4f6b1426be747e99420bed675d356caa02a2023b
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-09-02 12:52:12 +0200

    search field

--- pages/search.js
+++ pages/search.js
@@ -15,13 +15,28 @@ export default function searchPage() {
     return <p>Failed to load plants!</p>;
   }
 
+  function handleSearch(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    console.log(data);
+  }
+
   return (
     <>
       <BackButton />
       <h3>add a new plant</h3>
       <input type="checkbox"></input>
       <label>hide owned</label>
-      <input type="text" placeholder="search for name/botanicalName"></input>
+      <form onSubmit={handleSearch}>
+        <input
+          name="query"
+          type="text"
+          placeholder="search for name/botanicalName"
+        ></input>
+        <button type="submit">Search Plant</button>
+      </form>
       <PlantList plants={data} session={session} />
       <button>Add a new one</button>
     </>
