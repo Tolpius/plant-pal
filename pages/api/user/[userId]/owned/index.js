ea4c9df299b2ff3f7d0dcaad682f200b4ad3fe23
Julius von Bonin <167006924+Tolpius@users.noreply.github.com>
2025-09-02 10:41:06 +0200

    added api method to add a completely new owned plant

--- pages/api/user/[userId]/owned/index.js
+++ pages/api/user/[userId]/owned/index.js
@@ -1,6 +1,7 @@
 import dbConnect from "@/db/dbConnect";
 import OwnedPlant from "@/db/models/OwnedPlant";
 import { getToken } from "next-auth/jwt";
+import Plant from "@/db/models/Plant";
 
 export default async function handler(request, response) {
   const { userId } = request.query;
@@ -15,19 +16,35 @@ export default async function handler(request, response) {
     return response.status(403).json({ error: "Access denied" });
   }
 
-  await dbConnect();
   try {
-
-  if (request.method === "GET") {
-      const plants = await OwnedPlant.find({userId});
-      if (!plants) {
-        return response.status(404).json({ error: "ownedPlants not found" });
+    await dbConnect();
+    switch (request.method) {
+      case "GET":
+        const plants = await OwnedPlant.find({ userId });
+        if (!plants) {
+          return response.status(404).json({ error: "ownedPlants not found" });
+        }
+        return response.status(200).json(plants);
+      case "POST": {
+        const plant = await Plant.create(request.body);
+        const ownedPlant = new OwnedPlant({
+          cataloguePlantId: plant._id,
+          userId: userId,
+          name: plant.name,
+          botanicalName: plant.botanicalName,
+          imageUrl: plant.imageUrl,
+          waterNeed: plant.waterNeed,
+          lightNeed: plant.lightNeed,
+          fertiliserSeasons: plant.fertiliserSeasons,
+          description: plant.description,
+        });
+        await ownedPlant.save();
+        return response.status(200).json(ownedPlant);
       }
-     return response.status(200).json(plants);
-    } 
+      default:
+        return response.status(405).json("Method not allowed");
     }
-    catch (error) {
-      response.status(500).json({ success: false, error: error.message });
+  } catch (error) {
+    response.status(500).json({ success: false, error: error.message });
   }
 }
-
