291dc58e976e1fd7c103f5abae9caa3d7b0f92db
dominikmusewitsch <dmusewitsch@gmail.com>
2025-09-05 10:48:42 +0200

    create model for Reminder

--- pages/reminders/index.js
+++ pages/reminders/index.js
@@ -1,3 +1,19 @@
+import styled from "styled-components";
+
 export default function Reminders() {
-  return <>Hello</>;
+  return (
+    <>
+      <StyledText>Reminders</StyledText>
+      <p>Today</p>
+      <p>Tomorrow</p>
+      <p>This Week</p>
+      <p>Next Week</p>
+    </>
+  );
 }
+
+const StyledText = styled.p`
+  padding-top: var(--padding-bg-sm);
+  text-align: center;
+  font-size: var(--font-size-xl);
+`;
