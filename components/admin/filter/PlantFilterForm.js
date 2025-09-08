db418bd9fb9fc17e36ffc1bdde479fcca703981d
Julius von Bonin <167006924+Tolpius@users.noreply.github.com>
2025-09-08 15:40:48 +0200

    Fixed filter form unnecessary div

--- components/admin/filter/PlantFilterForm.js
+++ components/admin/filter/PlantFilterForm.js
@@ -2,27 +2,25 @@ import styled from "styled-components";
 
 export default function PlantFilterForm({ onSubmit }) {
   return (
-    <StyledFilterForm>
-      <form onSubmit={onSubmit}>
-        <StyledVisibilityWrapper>
-          <legend>Public/Private:</legend>
-          <label>
-            <input type="radio" name="isPublic" value="true" />
-            <span>Show Public</span>
-          </label>
-          <label>
-            <input type="radio" name="isPublic" value="false" />
-            <span>Show Private</span>
-          </label>
-          <label>
-            <input type="radio" name="isPublic" value="all" defaultChecked />
-            <span>Show All</span>
-          </label>
-        </StyledVisibilityWrapper>
-        <StyledButtonWrapper>
-          <StyledButton type="submit">apply</StyledButton>
-        </StyledButtonWrapper>
-      </form>
+    <StyledFilterForm onSubmit={onSubmit}>
+      <StyledVisibilityWrapper>
+        <legend>Public/Private:</legend>
+        <label>
+          <input type="radio" name="isPublic" value="true" />
+          <span>Show Public</span>
+        </label>
+        <label>
+          <input type="radio" name="isPublic" value="false" />
+          <span>Show Private</span>
+        </label>
+        <label>
+          <input type="radio" name="isPublic" value="all" defaultChecked />
+          <span>Show All</span>
+        </label>
+      </StyledVisibilityWrapper>
+      <StyledButtonWrapper>
+        <StyledButton type="submit">apply</StyledButton>
+      </StyledButtonWrapper>
     </StyledFilterForm>
   );
 }
