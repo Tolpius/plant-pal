2e7a5158771c504f7ea1afa2311218124fb1cf8a
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-09-09 11:08:59 +0200

    reverted app

--- pages/_app.js
+++ pages/_app.js
@@ -3,33 +3,19 @@ import GlobalStyle from "../styles";
 import { SWRConfig } from "swr";
 import { SessionProvider } from "next-auth/react";
 
-async function fetcher(resource, init) {
-  const response = await fetch(resource, init);
-
-  if (!response.ok) {
-    const error = new Error("An error occurred while fetching the data.");
-
-    try {
-      error.info = await response.json();
-    } catch {
-      error.info = null;
-    }
-
-    error.status = response.status;
-    throw error;
-  }
-
-  const data = await response.json();
-  return data;
-}
-
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
     <SessionProvider session={session}>
-      <SWRConfig value={{ fetcher: fetcher }}>
+      <SWRConfig
+        value={{
+          refreshInterval: 3000,
+          fetcher: (resource, init) =>
+            fetch(resource, init).then((response) => response.json()),
+        }}
+      >
         <Layout>
           <GlobalStyle />
           <Component {...pageProps} />
