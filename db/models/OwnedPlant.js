f9bb3e6ae4771df3fb4ec28f967b99f30dd7e3bb
dominikmusewitsch <dmusewitsch@gmail.com>
2025-09-12 11:02:16 +0200

    adjust OwnedPlant schema

--- db/models/OwnedPlant.js
+++ db/models/OwnedPlant.js
@@ -3,15 +3,17 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 const ownedPlantSchema = new Schema(
   {
-    cataloguePlantId: { type: String, required: true },
+    cataloguePlantId: {
+      type: Schema.Types.ObjectId,
+      ref: "Plant",
+      required: true,
+    },
     userId: { type: String, required: true },
-    name: { type: String, required: true },
-    botanicalName: { type: String, required: true },
-    imageUrl: { type: String, required: true },
-    waterNeed: { type: String, required: true },
-    lightNeed: { type: String, required: true },
-    fertiliserSeasons: { type: [String], required: true },
-    description: { type: String, required: true },
+    nickname: { type: String },
+    location: { type: String },
+    userImageUrl: { type: String },
+    acquiredDate: { type: Date },
+    notes: { type: String },
   },
   { timestamps: true }
 );
