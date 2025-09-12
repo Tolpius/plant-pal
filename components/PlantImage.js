ea7075d551775fe18287adb8ad88705c34d6ce06
Julius von Bonin <167006924+Tolpius@users.noreply.github.com>
2025-09-12 14:14:15 +0200

    created a plantImage component with fallbacks

---
+++ components/PlantImage.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Image from "next/image";
+
+const PlantImage = ({ plant, alt = "Plant", width = 200, height = 200, className }) => {
+  const imageOptions = [
+    plant?.storedImageUrl,
+    plant?.imageUrl,
+    plant?.cataloguePlantId?.storedImageUrl,
+    plant?.cataloguePlantId?.imageUrl,
+    "/defaultImage.png",
+  ].filter(Boolean); 
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleError = () => {
+    if (currentIndex < imageOptions.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  return (
+    <Image
+      src={imageOptions[currentIndex]}
+      alt={alt}
+      width={width}
+      height={height}
+      className={className}
+      onError={handleError}
+    />
+  );
+};
+
+export default PlantImage;
