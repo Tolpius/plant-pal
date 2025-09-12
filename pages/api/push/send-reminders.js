5e5df207b7ddad4fd2e5bad3ef970b834621f628
Johanna Hammerl <johanna_hammerl@t-online.de>
2025-09-12 16:09:38 +0200

    add send-reminders endpoint

---
+++ pages/api/push/send-reminders.js
@@ -0,0 +1,78 @@
+import dbConnect from "@/db/dbConnect";
+import Reminder from "@/db/models/Reminder";
+import Subscription from "@/db/models/Subscription";
+import webpush from "web-push";
+
+export default async function handler(request, response) {
+  if (request.method !== "POST") return response.status(405).end();
+
+  await dbConnect();
+
+  try {
+    const allReminders = await Reminder.find();
+
+    const now = new Date();
+    const filteredReminders = allReminders.filter((reminder) => {
+      const result = reminder.dueDate > now;
+      console.debug(`${reminder.dueDate} === ${now}? ${result}`);
+      return result;
+    });
+
+    console.log(
+      `Found ${filteredReminders.length} triggered reminders to push notifications`
+    );
+
+    if (filteredReminders.length === 0) {
+      return response.status(200).json({ sent: 0 });
+    }
+
+    const publicKey = process.env.VAPID_PUBLIC_KEY;
+    const privateKey = process.env.VAPID_PRIVATE_KEY;
+    if (!publicKey || !privateKey) {
+      console.error("VAPID keys missing");
+      return response.status(500).json({});
+    }
+
+    webpush.setVapidDetails("mailto:joh.hammerl@web.de", publicKey, privateKey);
+
+    let notificationsSentAmount = 0;
+    for (const reminder of filteredReminders) {
+      const subs = await Subscription.find({ userId: reminder.userId });
+
+      console.debug(
+        `found ${subs.length} subscriptions for reminder ${reminder._id} from user ${reminder.userId}`
+      );
+
+      for (const subscription of subs) {
+        try {
+          const pushSubscription = {
+            endpoint: subscription.endpoint,
+            keys: {
+              p256dh: subscription.p256dh,
+              auth: subscription.auth,
+            },
+          };
+          const payload = {
+            title: "Plant Pal - Reminder",
+            body: `${reminder.title} your plant`,
+            icon: "/icon.png",
+            url: "/reminders",
+            tag: reminder._id.toString(),
+          };
+          await webpush.sendNotification(
+            pushSubscription,
+            JSON.stringify(payload)
+          );
+          notificationsSentAmount++;
+        } catch (error) {
+          console.error(error);
+        }
+      }
+    }
+
+    return response.status(200).json({ sent: notificationsSentAmount });
+  } catch (error) {
+    console.error(error);
+    return response.status(500).json({ error: error.message }).end();
+  }
+}
