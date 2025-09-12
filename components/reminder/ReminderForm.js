adb8edaabd240efba67625eb34c14917f6290654
dominikmusewitsch <dmusewitsch@gmail.com>
2025-09-12 15:21:28 +0200

    further debugging of schema

--- components/reminder/ReminderForm.js
+++ components/reminder/ReminderForm.js
@@ -85,9 +85,11 @@ export default function ReminderForm({ userId, reminderId }) {
           <option value="">Select a plant</option>
           {plants.map((plant) => (
             <option key={plant._id} value={plant._id}>
-              {plant.name?.trim()
-                ? plant.name
-                : plant.cataloguePlantId?.name || "Unnamed plant"}
+              {plant?.nickname && plant.nickname.trim() !== ""
+                ? plant.nickname
+                : plant?.cataloguePlantId?.name?.trim() !== ""
+                ? plant.cataloguePlantId.name
+                : "Unknown plant"}
             </option>
           ))}
         </Select>
